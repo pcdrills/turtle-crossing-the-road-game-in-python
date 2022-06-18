d4ef7f9ea01c8aac75f8b64f073f5b8e4b2ad1dd
pcdrills <pcdrillsofficial@gmail.com>
2022-06-18 07:31:07 +0100

    Created the player, which is the turtle and implemented the movement, created the car manager and made the cars move to the left accordingly.

---
+++ player.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+
+STARTING_POSITION = (0, -200)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 200
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+    
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
